4cfa9b3758e3cba7986dc0009039db04839b11f1
Yoshi Hemzal <yhem7754@uni.sydney.edu.au>
2018-08-04 18:16:40 +1000

    Added Token limits for stages
    - Added logic for switching tiers manually, if not, use intervals

--- KEY/contracts/TokenSale.sol
+++ KEY/contracts/TokenSale.sol
@@ -14,6 +14,9 @@ contract TokenSale is KEYToken {
 	using SafeMath for uint256;
 
 	address public owner;
+	uint256 birth;
+	uint256[3] stages;
+
 	uint8 public currentTier = 0; // Current Pricing Tier.
 	bool public enableSale = true;
 	uint256 public tokensSold = 0;
@@ -30,10 +33,16 @@ contract TokenSale is KEYToken {
 
 
 	mapping(uint8 => uint256) public tierToRates;
+	mapping(uint8 => uint256) public tierToLimits;
 
 	constructor() public {
 		owner = msg.sender;
 		balances[this] = totalSupply;
+		birth = now;
+
+		stages[0] = birth + (4 weeks);
+		stages[1] = birth + (2 * (4 weeks));
+		stages[2] = birth + (3 * (4 weeks));
 
 		investorAlloc = ((totalSupply * investorsNumerator) / 100) / 1 ether;
 		teamsAlloc = ((totalSupply * teamNumerator) / 100) / 1 ether;
@@ -50,7 +59,7 @@ contract TokenSale is KEYToken {
 		_;
 	}
 
-	// Get total amount tokens purchased
+	// Get total amount tokens  purchased
 	function getTokensSold() public view returns (uint256 total) {
 		return tokensSold;
 	}
@@ -77,10 +86,22 @@ contract TokenSale is KEYToken {
 		return true;
 	}
 
-	/* Switch to the next tier
-	* NOTE: In the final contract, this will most likely happen automatically
-	* using a DateTime library, depending on gas efficiency. Decision pending.
-	*/
+	// Set inital tier limit
+	function setInitialTierLimit(uint8 _tierLimitPercent) public onlyOwner saleOngoing returns (bool success) {
+		require(currentTier == 0);
+		tierToLimits[0] = (investorAlloc * _tierLimitPercent) / 100;
+		return true;
+	}
+
+	// Set tier limits
+	function setTierLimits(uint8 _tier1LimitPercent, uint8 _tier2LimitPercent, uint8 _tier3LimitPercent) public onlyOwner saleOngoing returns (bool success) {
+		tierToLimits[1] = (investorAlloc * _tier1LimitPercent) / 100;
+		tierToLimits[2] = (investorAlloc * _tier2LimitPercent) / 100;
+		tierToLimits[3] = (investorAlloc * _tier3LimitPercent) / 100;
+		return true;
+	}
+
+	// Switch to the next tier
 	function switchTiers(uint8 _tier) public onlyOwner returns (bool success) {
 		require(_tier == 1 || _tier == 2 || _tier == 3);
 		require(_tier > currentTier);
@@ -95,9 +116,36 @@ contract TokenSale is KEYToken {
 
 	// Token purchase function. Tokens can ONLY be purchase using this method
 	function buyTokens() public saleOngoing payable {
-		uint256 quantity = (msg.value.mul(tierToRates[currentTier])).div(1 ether);
+		uint256 quantity = 0;
+		uint8 stage = 0;
 
-		// Check if there are enough tokens to sell
+		// If tier is manually set (if all tokens in a stage are sold), then calculate rate accordingly
+		if (currentTier > 0) {
+			quantity = (msg.value.mul(tierToRates[currentTier])).div(1 ether);
+			stage = currentTier;
+		}
+
+		// Stage 1; 30% Bonus
+		else if (now < stages[0]) {
+			quantity = (msg.value.mul(tierToRates[0])).div(1 ether);
+		}
+		// Stage 2; 20% Bonus
+		else if (now < stages[1]) {
+			quantity = (msg.value.mul(tierToRates[1])).div(1 ether);
+			stage = 1;
+		}
+		// Stage 3; 10% Bonus
+		else if (now < stages[2]) {
+			quantity = (msg.value.mul(tierToRates[2])).div(1 ether);
+			stage = 2;
+		}
+		// Stage 4; No Bonus
+		else {
+			quantity = (msg.value.mul(tierToRates[3])).div(1 ether);
+			stage = 3;
+		}
+
+		// Check if there are enough tokens in current stage to sell
 		require(investorAlloc.sub(tokensSold) >= quantity);
 
 		balances[msg.sender] = balances[msg.sender].add(quantity);
@@ -123,7 +171,7 @@ contract TokenSale is KEYToken {
 		// Return any unsold tokens to contract owner
 		uint256 remaining = investorAlloc.sub(tokensSold);
 		emit Transfer(this, owner, remaining);
-		
+
 		return true;
 	}
 }
